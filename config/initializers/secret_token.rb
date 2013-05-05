1320fdcd451d00022213c7dbfb4565e6dbb58e8e
Brent Harding <brent@hostany.net>
2013-05-05 06:33:40 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GainBlog::Application.config.secret_token = '869b9230cd3a2ed0b6b2641e58f52e0c2e0ca4d5c651a2c9c3fb8035d9e7600c5d6db4965685b79cad575fc9436ef84223e225119f196c72d83dd180fc0801ce'
